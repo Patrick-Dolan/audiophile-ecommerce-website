1f3b1d6a11daf150f8f041daf086f53fbc2f4b37
Patrick Dolan <dolanp1992@gmail.com>
2024-04-19 14:18:22 -0700

    add github pages deployment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <h1 className="text-center">Audiophile site</h1>
+    <>
+      <h1 className="text-center">Audiophile site</h1>
+      <p className="text-center">Deployment test</p>
+    </>
   );
 }
 
