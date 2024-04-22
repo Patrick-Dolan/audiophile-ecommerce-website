f7f6cfb1b597235f93935ccf3e9f9e1c1f1b8866
Patrick Dolan <dolanp1992@gmail.com>
2024-04-22 13:53:48 -0700

    add react-router and basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-center">Audiophile site</h1>
-      <p className="text-center">Deployment test</p>
+      <header className="mx-6">
+        <h1 className="text-center">Audiophile site</h1>
+      </header>
+      <div className="mx-6">
+        <Navbar />
+      </div>
+      <main className="mx-6">
+        <Routes>
+          <Route path="/audiophile-ecommerce-website/" element={<h1>Home</h1>} />
+          <Route path="/audiophile-ecommerce-website/category/headphones" element={<h1>headphones</h1>} />
+          <Route path="/audiophile-ecommerce-website/category/speakers" element={<h1>speakers</h1>} />
+          <Route path="/audiophile-ecommerce-website/category/earphones" element={<h1>earphones</h1>} />
+          <Route path="/audiophile-ecommerce-website/checkout" element={<h1>checkout</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </main>
     </>
   );
 }
