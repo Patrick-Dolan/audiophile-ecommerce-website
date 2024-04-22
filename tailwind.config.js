1525d48414b51f72954704d1573015cb9afa1261
Patrick Dolan <dolanp1992@gmail.com>
2024-04-22 08:57:23 -0700

    add colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#D87D4a",
+        "primary-light": "#FBAF85",
+        "off-black": "#101010",
+        "off-grey": "#F1F1F1",
+        "off-white": "#FAFAFA",
+      }
+    },
   },
   plugins: [],
 }
-
