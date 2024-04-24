f3b6b4a7f1a056e06d2ffad1786db80d1e2dcbc8
Patrick Dolan <dolanp1992@gmail.com>
2024-04-24 10:09:43 -0700

    add homepage component

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import ContentWrapper from "../shared/ContentWrapper"
+
+function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <ContentWrapper>
+        <p>This is the home page content</p>
+      </ContentWrapper>
+    </>
+  )
+}
+
+export default HomePage
