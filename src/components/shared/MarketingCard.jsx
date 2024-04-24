d3f6ff69af073a10447f446c7b457969fe9c8d00
Patrick Dolan <dolanp1992@gmail.com>
2024-04-24 10:29:52 -0700

    add MarketingCard component

---
+++ src/components/shared/MarketingCard.jsx
@@ -0,0 +1,11 @@
+function MarketingCard() {
+  return (
+    <article className="text-center mb-30">
+      <img src="/audiophile-ecommerce-website/shared/mobile/image-best-gear.jpg" alt="A man wearing headphones" className="rounded-lg mb-10" />
+      <h2 className="mb-8">Bringing you the <span className="text-primary">best</span> audio gear</h2>
+      <p className="opacity-50">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+    </article>
+  )
+}
+
+export default MarketingCard
