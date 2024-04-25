47e024ee575bc1b3158da520f66b567bcdbb1422
Patrick Dolan <dolanp1992@gmail.com>
2024-04-25 09:27:25 -0700

    add flex to make image more responsive

--- src/components/shared/MarketingCard.jsx
+++ src/components/shared/MarketingCard.jsx
@@ -1,6 +1,6 @@
 function MarketingCard() {
   return (
-    <article className="text-center mb-30">
+    <article className="text-center mb-30 flex flex-col">
       <img src="/audiophile-ecommerce-website/shared/mobile/image-best-gear.jpg" alt="A man wearing headphones" className="rounded-lg mb-10" />
       <h2 className="mb-8">Bringing you the <span className="text-primary">best</span> audio gear</h2>
       <p className="opacity-50">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
