2ebb0475d5d0c8524d5265cfd0a6547b6365e6a1
Patrick Dolan <dolanp1992@gmail.com>
2024-04-25 08:48:25 -0700

    refactor navbar to have better state names and conditional rendering

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -4,14 +4,14 @@ import NavLinks from "./NavLinks";
 
 function Navbar() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
-  const [show, setShow] = useState(true);
+  const [showNavBar, setShowNavBar] = useState(true);
   const [scrollPos, setScrollPos] = useState(0);
   const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
       setScrollPos(document.body.getBoundingClientRect().top);
-      setShow(document.body.getBoundingClientRect().top > scrollPos);
+      setShowNavBar(document.body.getBoundingClientRect().top > scrollPos);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -35,27 +35,32 @@ function Navbar() {
     setMobileNavOpen(false);
   }
 
-  return (
-    <>
-      <nav className={`w-full z-30 fixed ${show ? 'fixed' : 'hidden'} ${mobileNavOpen ? "rounded-b-md" : ""} ${location.pathname === "/audiophile-ecommerce-website/" && scrollPos === 0 ? "bg-transparent" : "bg-black"}`}>
-        <div className="flex justify-between items-center px-6 min-h-22.5 border-b border-white border-opacity-10">
-          <button onClick={mobileNavOpen ? closeMobileNav : openMobileNav}>
-            <img src="/audiophile-ecommerce-website/shared/tablet/icon-hamburger.svg" alt="Hamburger icon for navigation menu." />
-          </button>
-          <Link to="/audiophile-ecommerce-website/" onClick={closeMobileNav}>
-            <img src="/audiophile-ecommerce-website/shared/desktop/logo.svg" alt="audiophile logo." />
-          </Link>
-          <button onClick={() => alert("Cart clicked")}>
-            <img src="/audiophile-ecommerce-website/shared/desktop/icon-cart.svg" alt="Cart icon button." />
-          </button>
-        </div>
-        <NavLinks mobileNavOpen={mobileNavOpen} closeMobileNav={closeMobileNav} />
-      </nav>
-      <div onClick={closeMobileNav} className={`fixed inset-0 bg-black opacity-40 z-20 ${mobileNavOpen ? "" : "hidden"}`}></div>
-      {/* Following div is designed to push elements down below fixed nav height */}
-      {location.pathname !== "/audiophile-ecommerce-website/" && <div className="min-h-22.5"></div>}
-    </>
-  )
+  if (!showNavBar) {
+    return null;
+  } else {
+    return (
+      <>
+        <nav className={`w-full z-30 fixed ${mobileNavOpen ? "rounded-b-md" : ""} ${location.pathname === "/audiophile-ecommerce-website/" && scrollPos === 0 ? "bg-transparent" : "bg-black"}`}>
+          <div className="flex justify-between items-center px-6 min-h-22.5 border-b border-white border-opacity-10">
+            <button onClick={mobileNavOpen ? closeMobileNav : openMobileNav}>
+              <img src="/audiophile-ecommerce-website/shared/tablet/icon-hamburger.svg" alt="Hamburger icon for navigation menu." />
+            </button>
+            <Link to="/audiophile-ecommerce-website/" onClick={closeMobileNav}>
+              <img src="/audiophile-ecommerce-website/shared/desktop/logo.svg" alt="audiophile logo." />
+            </Link>
+            <button onClick={() => alert("Cart clicked")}>
+              <img src="/audiophile-ecommerce-website/shared/desktop/icon-cart.svg" alt="Cart icon button." />
+            </button>
+          </div>
+          {mobileNavOpen && <NavLinks handleMobileNav={closeMobileNav} />}
+        </nav>
+        <div onClick={closeMobileNav} className={`fixed inset-0 bg-black opacity-40 z-20 ${mobileNavOpen ? "" : "hidden"}`}></div>
+        {/* Following div is designed to push elements down below fixed nav height */}
+        {location.pathname !== "/audiophile-ecommerce-website/" && <div className="min-h-22.5"></div>}
+      </>
+    )
+  }
+
 }
 
 export default Navbar
