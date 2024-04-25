cfb87539b1316f4eba8d6939cc9dc37757fa2e19
Patrick Dolan <dolanp1992@gmail.com>
2024-04-25 09:17:28 -0700

    add NavLinks and element spacing

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import HomepageHero from "../homepage/HomepageHero"
 import ContentWrapper from "../shared/ContentWrapper"
 import MarketingCard from "../shared/MarketingCard"
+import NavLinks from "../navigation/NavLinks"
 
 function HomePage() {
   return (
     <>
-      <div className="mb-10">
+      <div className="mb-2">
         <HomepageHero />
       </div>
       <ContentWrapper>
-        <MarketingCard />
+        <div className="space-y-30 mb-30">
+          <NavLinks />
+          <MarketingCard />
+        </div>
       </ContentWrapper>
     </>
   )
