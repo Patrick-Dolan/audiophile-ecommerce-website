6523426873a6dbd743f118ae3805154e628eebcf
Patrick Dolan <dolanp1992@gmail.com>
2024-05-01 09:05:31 -0700

    adjust spacing and add placeholders

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -6,12 +6,17 @@ import NavLinks from "../navigation/NavLinks"
 function HomePage() {
   return (
     <>
-      <div className="mb-2">
-        <HomepageHero />
-      </div>
+      <HomepageHero />
       <ContentWrapper>
-        <div className="space-y-30 mb-30">
+        <div className="mb-22.5">
           <NavLinks />
+        </div>
+        <div className="space-y-30 mb-30">
+          <div className="space-y-6">
+            <h1 className="text-center">Card 1 placeholder</h1>
+            <h1 className="text-center">Card 2 placeholder</h1>
+            <h1 className="text-center">Card 3 placeholder</h1>
+          </div>
           <MarketingCard />
         </div>
       </ContentWrapper>
