fdfd158e42072880b508b500abf4e584be4ee4b7
Patrick Dolan <dolanp1992@gmail.com>
2024-05-06 13:11:04 -0700

    add spacing value

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ export default {
         "22": "5.5rem", // 88px @ 16px base
         "22.5": "5.625em", // 90px @ 16px base
         "25": "6.25rem", // 100px @ 16px base
+        "25.5": "6.375rem", // 102px @ 16px base
         "27": "6.75rem", // 108px @ 16px base
         "28": "7rem", // 112px @ 16px base
         "30": "7.5rem", // 120px @ 16px base
