80aebfde858e6de7ce02e5a5c946f99dface4b9c
Patrick Dolan <dolanp1992@gmail.com>
2024-05-15 10:09:00 -0700

    refactor ADD_TO_CART to use addProductsToCart function and add state fix for REMOVE_ONE_FROM_CART

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,6 +1,7 @@
 import { calculateCartSubtotal } from "./utils/calculateCartSubtotal";
 import { calculateGrandTotal } from "./utils/calculateGrandTotal";
 import { calculateVAT } from "./utils/calculateVAT";
+import { addProductsToCart } from "./utils/addProductsToCart";
 import * as constants from "../actions/actionTypes";
 
 export const createInitialCartState = () => ({
@@ -14,18 +15,7 @@ export const createInitialCartState = () => ({
 export const cartReducer = (state = createInitialCartState(), action) => {
   switch (action.type) {
     case constants.ADD_TO_CART: {
-      let updatedProducts = [];
-      const existingProduct = state.products.find(product => product.id === action.payload.id);
-      if (existingProduct !== undefined) {
-        updatedProducts = state.products.map(product => {
-          if (product.id === action.payload.id) {
-            return { ...product, quantity: product.quantity + 1 };
-          }
-          return product;
-        });
-      } else {
-        updatedProducts = [...state.products, { ...action.payload, quantity: 1 }];
-      }
+      const updatedProducts = addProductsToCart(state.products, action);
       const updatedSubtotal = calculateCartSubtotal(updatedProducts);
       const updatedVat = calculateVAT(updatedSubtotal);
       const updatedGrandTotal = calculateGrandTotal(updatedSubtotal, state.shippingCost);
@@ -38,8 +28,12 @@ export const cartReducer = (state = createInitialCartState(), action) => {
       };
     }
     case constants.REMOVE_ONE_FROM_CART: {
+      const productFromCart = state.products.find(product => product.id === action.payload.id);
+      if (productFromCart === undefined) {
+        return state;
+      }
       let updatedProducts = [];
-      if (state.products.find(product => product.id === action.payload.id).quantity === 1) {
+      if (productFromCart.quantity === 1) {
         updatedProducts = state.products.filter(product => product.id !== action.payload.id);
       } else {
         updatedProducts = state.products.map(product => {
