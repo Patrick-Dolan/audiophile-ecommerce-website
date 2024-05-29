d246648739698e628fd225926348176ab1a3510a
Patrick Dolan <dolanp1992@gmail.com>
2024-05-29 15:38:16 -0700

    refactor for tablet responsiveness

--- src/components/products/RecommendedProducts.jsx
+++ src/components/products/RecommendedProducts.jsx
@@ -1,20 +1,32 @@
-import PropTypes from 'prop-types'
-import Button from '../shared/Button'
-import { Link } from 'react-router-dom'
+import PropTypes from "prop-types"
+import Button from "../shared/Button"
+import { Link } from "react-router-dom"
+import useWindowWidth from "../../hooks/useWindowWidth"
 
 function RecommendedProducts({ products }) {
+  const windowWidth = useWindowWidth();
+
+  let imageSize = "mobile";
+  if (windowWidth >= 768 && windowWidth < 1440) {
+    imageSize = "tablet";
+  } else if (windowWidth >= 1440) {
+    imageSize = "desktop";
+  }
+
   return (
     <div className="text-center">
-      <h3 className="mb-10">You may also like</h3>
-      {products.map((product, i) => (
-        <div key={i} className="mb-14">
-          <img src={product.image.mobile} alt={product.name} className="rounded-lg mb-8" />
-          <h5 className="mb-8">{product.name}</h5>
-          <Link to={`/audiophile-ecommerce-website/product/${product.slug}`}>
-            <Button text="See Product" type="button" />
-          </Link>
-        </div>
-      ))}
+      <h3 className="mb-10 md:text-8 md:mb-13">You may also like</h3>
+      <div className="md:flex md:gap-3">
+        {products.map((product, i) => (
+          <div key={i} className="mb-14 md:mb-0">
+            <img src={product.image[imageSize]} alt={product.name} className="rounded-lg mb-8 md:mb-10" />
+            <h5 className="mb-8">{product.name}</h5>
+            <Link to={`/audiophile-ecommerce-website/product/${product.slug}`} className="flex justify-center">
+              <Button text="See Product" type="button" />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
