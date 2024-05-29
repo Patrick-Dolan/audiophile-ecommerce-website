cfcafb2f29487e7f4943a1b80c36e9f4536a794a
Patrick Dolan <dolanp1992@gmail.com>
2024-05-29 10:35:13 -0700

    refactor SecondaryProductCard to be responsive for tablets

--- src/components/homepage/SecondaryProductCard.jsx
+++ src/components/homepage/SecondaryProductCard.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom"
+import useWindowWidth from "../../hooks/useWindowWidth"
 import Button from "../shared/Button"
-import ContentWrapper from "../shared/ContentWrapper"
 
 function SecondaryProductCard() {
+  const windowWidth = useWindowWidth();
+  let imageUrl = "/audiophile-ecommerce-website/home/mobile/image-earphones-yx1.jpg";
+  if (windowWidth >= 768 && windowWidth < 1440) {
+    imageUrl = "/audiophile-ecommerce-website/home/tablet/image-earphones-yx1.jpg";
+  } else if (windowWidth >= 1440) {
+    imageUrl = "/audiophile-ecommerce-website/home/desktop/image-earphones-yx1.jpg";
+  }
+
   return (
-    <article className="space-y-6">
-      <img src="/audiophile-ecommerce-website/home/mobile/image-earphones-yx1.jpg" alt="YX1 Earphones" className="rounded-lg w-full"/>
-        <div className="min-h-50 bg-off-grey rounded-lg">
-        <ContentWrapper>
-          <div className="space-y-8 py-10">
+    <article className="grid grid-cols-1 md:grid-cols-2 gap-3">
+      <img src={imageUrl} alt="YX1 Earphones" className="rounded-lg w-full"/>
+      <div className="min-h-50 bg-off-grey rounded-lg md:min-h-0">
+        <div className="mx-6 md:mx-10">
+          <div className="space-y-8 py-10 md:py-25">
             <h2>YX1 Earphones</h2>
             <div>
               <Link to="/audiophile-ecommerce-website/product/yx1-earphones">
@@ -16,8 +24,8 @@ function SecondaryProductCard() {
               </Link>
             </div>
           </div>
-        </ContentWrapper>
         </div>
+      </div>
     </article>
   )
 }
