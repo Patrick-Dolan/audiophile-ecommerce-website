6fa1d916cbcdebbf741b32ebe38a36a06874f491
Patrick Dolan <dolanp1992@gmail.com>
2024-05-30 15:40:22 -0700

    add desktop version of navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from "react-router-dom"
 import { useState, useEffect } from "react"
 import NavLinks from "./NavLinks";
 import Cart from "../cart/Cart";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 function Navbar() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
@@ -9,9 +10,13 @@ function Navbar() {
   const [showNavBar, setShowNavBar] = useState(true);
   const [scrollPos, setScrollPos] = useState(0);
   const location = useLocation();
+  const windowWidth = useWindowWidth();
 
   useEffect(() => {
     const handleScroll = () => {
+      if (cartOpen) {
+        closeCart();
+      }
       setTimeout(() => {
         setScrollPos(document.body.getBoundingClientRect().top);
         setShowNavBar(document.body.getBoundingClientRect().top > scrollPos);
@@ -59,6 +64,37 @@ function Navbar() {
 
   if (!showNavBar) {
     return null;
+  } else if (windowWidth >= 1440) {
+    return (
+      <>
+        <nav className={`w-full z-30 fixed ${location.pathname === "/audiophile-ecommerce-website/" && scrollPos === 0 ? "bg-transparent" : "bg-black"}`}>
+          <div className="grid grid-cols-3 justify-between items-center border-b border-white border-opacity-10 min-h-22.5 md:max-w-196.5 md:mx-auto lg:max-w-277.25 lg:mx-auto">
+            <div className="flex">
+              <Link to="/audiophile-ecommerce-website/">
+                <img src="/audiophile-ecommerce-website/shared/desktop/logo.svg" alt="audiophile logo." />
+              </Link>
+            </div>
+            <div className="flex gap-8 text-white">
+              <Link className="hover:text-primary" to="/audiophile-ecommerce-website/">Home</Link>
+              <Link className="hover:text-primary" to="/audiophile-ecommerce-website/category/headphones">Headphones</Link>
+              <Link className="hover:text-primary" to="/audiophile-ecommerce-website/category/speakers">Speakers</Link>
+              <Link className="hover:text-primary" to="/audiophile-ecommerce-website/category/earphones">Earphones</Link>
+            </div>
+            <div className="flex justify-end">
+              <button onClick={cartOpen ? closeCart : openCart}>
+                <img src="/audiophile-ecommerce-website/shared/desktop/icon-cart.svg" alt="Cart icon button." />
+              </button>
+            </div>
+          </div>
+          {cartOpen &&
+            <div className="fixed z-30 w-full">
+              <Cart closeCartOverlay={handleCloseOverlays} />
+            </div>
+          }
+        </nav>
+        <div onClick={handleCloseOverlays} className={`fixed inset-0 bg-black opacity-40 z-20 ${mobileNavOpen || cartOpen ? "" : "hidden"}`}></div>
+      </>
+    )
   } else {
     return (
       <>
