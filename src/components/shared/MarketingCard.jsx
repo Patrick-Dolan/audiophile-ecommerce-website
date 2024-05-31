0865017086b9198aff660aa70b54745a9c563170
Patrick Dolan <dolanp1992@gmail.com>
2024-05-31 11:18:04 -0700

    add large screen styles

--- src/components/shared/MarketingCard.jsx
+++ src/components/shared/MarketingCard.jsx
@@ -11,9 +11,9 @@ function MarketingCard() {
   }
 
   return (
-    <article className="text-center mb-30 flex flex-col">
-      <img src={imageUrl} alt="A man wearing headphones" className="rounded-lg mb-10 md:mb-16" />
-      <div className="md:max-w-143 md:mx-auto">
+    <article className="text-center mb-30 flex flex-col lg:flex-row-reverse lg:text-left">
+      <img src={imageUrl} alt="A man wearing headphones" className="rounded-lg mb-10 md:mb-16 lg:mb-0" />
+      <div className="md:max-w-143 md:mx-auto lg:mr-31 lg:my-auto">
         <h2 className="mb-8 md:text-10">Bringing you the <span className="text-primary">best</span> audio gear</h2>
         <p className="opacity-50">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
       </div>
